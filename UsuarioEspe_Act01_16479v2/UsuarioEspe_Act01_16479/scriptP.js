1ac457d352f73f95b47f94d3f90a9b7833f8f45c
Olalla Romero, Kris Sebastian <121052861+KrisOlalla@users.noreply.github.com>
2024-05-23 16:54:02 -0500

    Add files via upload

---
+++ UsuarioEspe_Act01_16479v2/UsuarioEspe_Act01_16479/scriptP.js
@@ -0,0 +1,9 @@
+document.getElementById('btnReset').addEventListener('click', function() {
+    // Obtener los formularios
+    const form1 = document.getElementById('form1');
+    const form2 = document.getElementById('form2');
+    
+    // Reiniciar los formularios
+    form1.reset();
+    form2.reset();
+});
