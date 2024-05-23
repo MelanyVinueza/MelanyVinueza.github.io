f7b8afa6908fce0b060c49785d8b9bd14c288638
Melany Vinueza <107556627+MelanyVinueza@users.noreply.github.com>
2024-05-23 17:23:42 -0500

    Delete UsuarioEspe_Act01_16479v2/UsuarioEspe_Act01_16479 directory

--- UsuarioEspe_Act01_16479v2/UsuarioEspe_Act01_16479/scriptP.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('btnReset').addEventListener('click', function() {
-    // Obtener los formularios
-    const form1 = document.getElementById('form1');
-    const form2 = document.getElementById('form2');
-    
-    // Reiniciar los formularios
-    form1.reset();
-    form2.reset();
-});
